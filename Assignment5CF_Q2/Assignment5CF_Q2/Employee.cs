c119c6e3b3cb8f0378f03067f83034b66d5d497b
RutujaThombre <thombre-rutuja.dilip@capgemini.com>
2022-12-28 17:59:50 +0530

    Assignment5CF_Q2

---
+++ Assignment5CF_Q2/Assignment5CF_Q2/Employee.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace Assignment5CF_Q2
+{
+    internal class Employee
+    {
+
+
+        ArrayList arrayList = new ArrayList();
+     
+        public void Accept(int id, string name, double salary, string JoiningDt, string department)
+        {
+            arrayList1.Add(id);
+            arrayList1.Add(name);
+            arrayList1.Add(department);
+            arrayList1.Add(JoiningDt);
+            arrayList1.Add(salary);
+
+        }
+        public void display()
+        {
+
+            Console.WriteLine("\nEmployee Details");
+            foreach (var item in arrayList1)
+            {
+                Console.WriteLine(item); 
+            }
+        }
+
+
+    }
+}
---
+++ Assignment5CF_Q2/Assignment5CF_Q2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Assignment5CF_Q2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp1 = new Employee();
+            emp1.Accept(101, "Nikita", 25000, "8 dec 2022", "Sales");
+
+            emp1.Accept(101, "Ram", 25000, "1 mar 2022", "Sales");
+
+            emp1.Accept(101, "Vijay", 25000, "7 nov 2022", "Sales");
+
+            emp1.display();
+
+        }
+    }
+}
