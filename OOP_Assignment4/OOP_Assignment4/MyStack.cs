f83230916f18286db8feff062523ed46beff71eb
RutujaThombre <thombre-rutuja.dilip@capgemini.com>
2022-12-28 17:10:37 +0530

    Assignment4

---
+++ OOP_Assignment4/OOP_Assignment4/MyStack.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Text;
+
+namespace OOP_Assignment4
+{
+    
+    
+        public class StackException : Exception
+        {
+            public StackException(string message) : base(message) { }
+        }
+        public class MyStack : ICloneable
+        {
+            private int[] arr = new int[20];
+            private int top = -1;
+            private int size;
+
+            public object Clone()
+            {
+                Console.WriteLine("----cloned Array----");
+                MyStack newStack = new MyStack
+                {
+                    arr = this.arr,
+                    top = this.top,
+                    size = this.size
+                };
+                return newStack;
+            }
+            public int SIZE { get { return size; } set { this.size = arr.Length; } }
+
+            public void PUSH(int a)
+            {
+                try
+                {
+                    if (top == arr.Length - 1)
+                    {
+                        throw new StackException("Fullstack Exception.");
+                    }
+                    arr[++top] = a;
+                    Console.WriteLine($"\npushed {a} onto the stack.");
+                }
+                catch (StackException st)
+                {
+                    Console.WriteLine(st);
+                }
+            }
+            public void POP()
+            {
+                try
+                {
+                    if (top == -1)
+                    {
+                        throw new StackException("Emptystack Exception.");
+                    }
+                    Console.WriteLine($"\n{arr[top]} popped.");
+                    arr[top--] = 0;
+                }
+                catch (StackException st)
+                {
+                    Console.WriteLine(st);
+                }
+            }
+            public void Display()
+            {
+                Console.WriteLine("\n---printing---");
+                foreach (int i in arr)
+                { 
+                    Console.WriteLine(i + "");
+                }
+            }
+        }
+    }
+
+
---
+++ OOP_Assignment4/OOP_Assignment4/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace OOP_Assignment4
+{
+    class Program
+    { 
+
+    static void Main(string[] args)
+        {
+        int n;
+            MyStack m = new MyStack();
+            do
+            {
+                Console.WriteLine("Enter Choice:");
+                Console.WriteLine("1.Push");
+                Console.WriteLine("2.Pop");
+                Console.WriteLine("3.Display");
+                Console.WriteLine("4.Clone");
+                Console.WriteLine("5.Exit");
+
+                n = Convert.ToInt32(Console.ReadLine());
+                switch (n)
+                {
+                    case 1:
+                        Console.WriteLine("Enter elements");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        m.PUSH(num);
+                        break;
+                    case 2:
+                        m.POP();
+                        break;
+                    case 3:
+                        m.Display();
+                        break;
+                    case 4:
+                        MyStack newStack = m.Clone() as MyStack;
+                        newStack.Display();
+                        break;
+                    case 5:
+                        break;
+                    default:
+                        Console.WriteLine("Enter valid choice");
+                        break;
+                }
+            } while (n < 6);
+
+        }
+    }    
+}
+            
+
+    
+
+        
