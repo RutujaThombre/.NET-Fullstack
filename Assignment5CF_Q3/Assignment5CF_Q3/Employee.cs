9e94686c3df5b8b22c20e4e3ce7a44f06f7304ce
RutujaThombre <thombre-rutuja.dilip@capgemini.com>
2023-01-03 00:04:10 +0530

    upload Assignment5CF_Q3

---
+++ Assignment5CF_Q3/Assignment5CF_Q3/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace Assignment_5_Q3
+{
+    class Employee
+    {
+        public int ID { get; set; }
+
+
+
+        public string Name { get; set; }
+        public string Department { get; set; }
+
+
+
+        public string Joining_Date { get; set; }
+        public int Salary { get; set; }
+    }
+}
