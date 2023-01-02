8bf57eecb06241f751b571bc8b2979066fbe06fc
RutujaThombre <thombre-rutuja.dilip@capgemini.com>
2023-01-02 22:45:09 +0530

    Upload SQL Assignment

---
+++ SQL/SQLAssignment-01.sql
@@ -0,0 +1,11 @@
+Select * From Customer
+
+ 
+
+Select Country 
+from Customer where Country like 'A%' or Country like 'I%'
+Order by Country
+
+ 
+
+Select FirstName from Customer where Firstname like '__u%'
